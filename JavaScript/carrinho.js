341e829fcdfc60e8df5a820551f7871cf362c12a
Daniel Costa Santos <122943882+danielcostasantoss@users.noreply.github.com>
2024-01-01 23:25:45 -0300

    att

---
+++ JavaScript/carrinho.js
@@ -0,0 +1,6 @@
+const removerProdutos = document.getElementsByClassName("remover-produto")
+for(var i = 0; i < removerProdutos.length; i++){
+    removerProdutos[i].addEventListener("click", function(event) {
+    
+    })
+}
